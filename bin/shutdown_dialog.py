45c70360c6dd38798a8d0a6e83b425ec6bce0214
Andrew Kravchuk <awkravchuk@gmail.com>
2017-02-12 12:29:36 +0300

    bin: shutdown-dialog style fixes

--- bin/shutdown_dialog.py
+++ bin/shutdown_dialog.py
@@ -33,117 +33,118 @@ service_name = 'org.ShutdownDialog'
 
 
 def set_icon(button, icon):
-        image = gtk.Image()
-        image.set_from_file(icon)
-        button.set_image(image)
+    image = gtk.Image()
+    image.set_from_file(icon)
+    button.set_image(image)
 
 
 class ShutdownDialog:
     def delete_event(self, widget, event, data=None):
-            gtk.main_quit()
-            return False
+        gtk.main_quit()
+        return False
 
     def keypress(self, widget, event):
-            if event.keyval == gtk.keysyms.Escape:
-                    gtk.main_quit()
+        if event.keyval == gtk.keysyms.Escape:
+            gtk.main_quit()
 
     def suspend(self, widget):
-            gtk.mainquit()
-            os.system("sudo pm-hibernate")
+        gtk.mainquit()
+        os.system('sudo pm-hibernate')
 
     def reboot(self, widget):
-            gtk.mainquit()
-            os.system("sudo reboot")
+        gtk.mainquit()
+        os.system('sudo reboot')
 
     def shutdown(self, widget):
-            gtk.mainquit()
-            os.system("sudo halt")
+        gtk.mainquit()
+        os.system('sudo halt')
 
     def __init__(self):
-            settings = gtk.settings_get_default()
-            settings.props.gtk_button_images = True
+        settings = gtk.settings_get_default()
+        settings.props.gtk_button_images = True
 
-            # Create a new window
-            self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-            self.window.set_title(" ")
-            self.window.set_resizable(False)
-            self.window.set_position(1)
-            self.window.connect("key-press-event", self.keypress)
-            self.window.connect("delete_event", self.delete_event)
-            self.window.set_border_width(10)
+        # Create a new window
+        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.window.set_title(' ')
+        self.window.set_resizable(False)
+        self.window.set_position(1)
+        self.window.connect('key-press-event', self.keypress)
+        self.window.connect('delete_event', self.delete_event)
+        self.window.set_border_width(10)
+        self.window.set_skip_taskbar_hint(True)
 
-            # Create a box to pack widgets into
-            self.box1 = gtk.HBox(False, 0)
-            self.window.add(self.box1)
+        # Create a box to pack widgets into
+        self.box1 = gtk.HBox(False, 0)
+        self.window.add(self.box1)
 
-            # Create cancel button
-            self.button1 = gtk.Button(" Cancel ")
-            set_icon(self.button1, cancel_icon)
-            self.button1.set_border_width(10)
-            self.button1.connect("clicked", self.delete_event,
-                                 "Changed me mind :)")
-            self.box1.pack_start(self.button1, True, True, 0)
-            self.button1.show()
+        # Create cancel button
+        self.button1 = gtk.Button(' Cancel ')
+        set_icon(self.button1, cancel_icon)
+        self.button1.set_border_width(10)
+        self.button1.connect('clicked', self.delete_event,
+                             'Changed me mind :)')
+        self.box1.pack_start(self.button1, True, True, 0)
+        self.button1.show()
 
-            # Create reboot button
-            self.button3 = gtk.Button(" Reboot ")
-            set_icon(self.button3, reboot_icon)
-            self.button3.set_border_width(10)
-            self.button3.connect("clicked", self.reboot)
-            self.box1.pack_start(self.button3, True, True, 0)
-            self.button3.show()
+        # Create reboot button
+        self.button3 = gtk.Button(' Reboot ')
+        set_icon(self.button3, reboot_icon)
+        self.button3.set_border_width(10)
+        self.button3.connect('clicked', self.reboot)
+        self.box1.pack_start(self.button3, True, True, 0)
+        self.button3.show()
 
-            # Create shutdown button
-            self.button4 = gtk.Button("Shutdown")
-            set_icon(self.button4, shutdown_icon)
-            self.button4.set_border_width(10)
-            self.button4.connect("clicked", self.shutdown)
-            self.box1.pack_start(self.button4, True, True, 0)
-            self.button4.show()
+        # Create shutdown button
+        self.button4 = gtk.Button('Shutdown')
+        set_icon(self.button4, shutdown_icon)
+        self.button4.set_border_width(10)
+        self.button4.connect('clicked', self.shutdown)
+        self.box1.pack_start(self.button4, True, True, 0)
+        self.button4.show()
 
-            # Create suspend button
-            self.button2 = gtk.Button(" Suspend ")
-            set_icon(self.button2, suspend_icon)
-            self.button2.set_border_width(10)
-            self.button2.connect("clicked", self.suspend)
-            self.box1.pack_start(self.button2, True, True, 0)
-            self.button2.show()
+        # Create suspend button
+        self.button2 = gtk.Button(' Suspend ')
+        set_icon(self.button2, suspend_icon)
+        self.button2.set_border_width(10)
+        self.button2.connect('clicked', self.suspend)
+        self.box1.pack_start(self.button2, True, True, 0)
+        self.button2.show()
 
     def show(self):
-            self.box1.show()
-            self.window.show()
-            self.window.present()
+        self.box1.show()
+        self.window.show()
+        self.window.present()
 
 
 class DbusService(dbus.service.Object):
-        '''
-        http://www.eurion.net/python-snippets/snippet/Single%20Instance.html
-        '''
-        def __init__(self, dialog):
-            self.dialog = dialog
-            bus_name = dbus.service.BusName(service_name,
-                                            bus=dbus.SessionBus())
-            dbus.service.Object.__init__(self, bus_name, '/org/ShutdownDialog')
+    '''
+    http://www.eurion.net/python-snippets/snippet/Single%20Instance.html
+    '''
+    def __init__(self, dialog):
+        self.dialog = dialog
+        bus_name = dbus.service.BusName(service_name,
+                                        bus=dbus.SessionBus())
+        dbus.service.Object.__init__(self, bus_name, '/org/ShutdownDialog')
 
-        @dbus.service.method(dbus_interface=service_name)
-        def show_window(self):
-            self.dialog.show()
+    @dbus.service.method(dbus_interface=service_name)
+    def show_window(self):
+        self.dialog.show()
 
 
 def main():
-        dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
-        if dbus.SessionBus().request_name(service_name) != \
-           dbus.bus.REQUEST_NAME_REPLY_PRIMARY_OWNER:
-                print("Shutdown dialog: already running")
-                method = dbus.SessionBus().get_object(
-                    service_name, '/org/ShutdownDialog').get_dbus_method(
-                        'show_window')
-                method()
-        else:
-                service = DbusService(ShutdownDialog())
-                service.show_window()
-                gtk.main()
+    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+    if dbus.SessionBus().request_name(service_name) != \
+       dbus.bus.REQUEST_NAME_REPLY_PRIMARY_OWNER:
+        print('Shutdown dialog: already running')
+        method = dbus.SessionBus().get_object(
+            service_name, '/org/ShutdownDialog').get_dbus_method(
+            'show_window')
+        method()
+    else:
+        service = DbusService(ShutdownDialog())
+        service.show_window()
+        gtk.main()
 
 
-if __name__ == "__main__":
-        main()
+if __name__ == '__main__':
+    main()
