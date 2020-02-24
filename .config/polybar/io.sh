6eff6f7bf42d8b015048eb4c9a0af3683c6741fc
Andrew Kravchuk <awkravchuk@gmail.com>
2020-02-24 08:44:07 +0300

    i3rc: minor polybar fixes and improvements

--- .config/polybar/io.sh
+++ .config/polybar/io.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-LANG=C iostat -md /dev/sda 1 | stdbuf -o0 cut -d \n -f 3 | stdbuf -o0 tr -s '\n\n' '\n' | stdbuf -o0 tr -s ' ' | stdbuf -o0 cut -d ' ' -f 3,4 | stdbuf -o0 sed 's/ / MB\/s %{F#9fafaf}↓%{F-}/'
+LANG=C iostat -ymd /dev/sda 1 1 | stdbuf -o0 cut -d \n -f 3 | stdbuf -o0 tr -s '\n\n' '\n' | stdbuf -o0 tr -s ' ' | stdbuf -o0 cut -d ' ' -f 3,4 | stdbuf -o0 sed 's/ / MB\/s %{F#9fafaf}↓%{F-}/' | stdbuf -o0 sed -n 3p
--- .config/polybar/procs.sh
+++ .config/polybar/procs.sh
@@ -4,4 +4,4 @@ procs=$(ps --ppid 2 -p 2 --deselect --sort -%cpu --no-headers -o state,comm | gr
 running=$(echo "$procs" | grep "^R" -c)
 all=$(echo "$procs" | wc -l)
 most=$(echo "$procs" | head -n1 | cut -d ' ' -f 2)
-echo "$all %{F#9fafaf}/%{F-} $running $most"
+printf "%-32s" "$all %{F#9fafaf}/%{F-} $running $most"
