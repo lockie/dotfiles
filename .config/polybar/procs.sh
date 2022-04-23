9931afa2291a3f071c2f8faa01cb860f469171c2
Andrew Kravchuk <awkravchuk@gmail.com>
2022-04-23 14:58:22 +0300

    i3rc: tiny polybar optimization

--- .config/polybar/procs.sh
+++ .config/polybar/procs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-procs=$(ps --ppid 2 -p 2 --deselect --sort -%cpu --no-headers -o state,comm | grep -v ps)
+procs=$(ps --ppid 2 -p 2 --deselect --sort -%cpu --no-headers -o state,comm | fgrep -v ps)
 running=$(echo "$procs" | grep "^R" -c)
 all=$(echo "$procs" | wc -l)
 most=$(echo "$procs" | head -n1 | cut -d ' ' -f 2)
