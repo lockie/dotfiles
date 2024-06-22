c16633ae4485d031c311235f96962d597a5ae4e6
Andrew Kravchuk <awkravchuk@gmail.com>
2024-06-22 17:27:28 +0200

    emacsrc: fixed white flash at early startup

---
+++ .emacs.d/early-init.el
@@ -0,0 +1,6 @@
+(setq default-frame-alist '(
+                            ;; Setting the face in here prevents flashes of
+                            ;; color as the theme gets activated
+                            (background-color . "#303030")
+                            (ns-appearance . dark)
+                            (ns-transparent-titlebar . t)))
